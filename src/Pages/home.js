bfec7fac7a9b90d5d1148854a2a125a2d12991d5
Dhyey Shah <83686569+DhyeyShah794@users.noreply.github.com>
2022-09-10 16:38:03 +0530

    Added apk file

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -3,6 +3,10 @@ import '../Styles/home.css';
 const Home = () => {
   return (
     <div className="home">
+      <div>
+        To get WeHealth you need to download the app from the below link
+
+      </div>
       <div className="home-container">
         <img className="home-image" src={require('../Images/logo.jpg')} alt="home" />
         <button className="download-btn"><a href="" download>Download</a></button>
